f5b972cd142ebc61ea8dcf46e2eb834bcce64884
Victor <victor.kryukov@gmail.com>
2025-04-22 13:50:00 -0700

    Updated to 0.3.1: fix the :client argument for create

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule OpenaiResponses.MixProject do
   def project do
     [
       app: :openai_responses,
-      version: "0.3.0",
+      version: "0.3.1",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
